50e6e625c2081999550738653fd383633e8d856c
Henryminh129 <156094841+Henryminh129@users.noreply.github.com>
2024-04-10 21:44:16 +0700

    new

--- src/components/AddUserInfor.js
+++ src/components/AddUserInfor.js
@@ -1,63 +1,5 @@
 import React, { useState } from "react";
 
-// class AddUserInfor extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             name: '',
-//             address: ' ',
-//             age: Number
-//         }
-//     }
-
-
-
-
-//     handleOnChangeInput_Name = (event) => {
-//         this.setState({
-//             name: event.target.value,
-//         })
-//     }
-
-//     handleOnChangeInput_Age = (event) => {
-//         this.setState({
-//             age: event.target.value,
-//         })
-//     }
-//     handleOnSubmit = (event) => {
-//         event.preventDefault()
-//         this.props.handleAddNewUser({
-//             id: Math.floor((Math.random()) + 1),
-//             name: this.state.name,
-//             age: this.state.age
-//         })
-
-//     }
-
-//     render() {
-//         return (
-//             <div>
-//                 my name is {this.state.name} and I'm {this.state.age}
-//                 <form onSubmit={(event) => this.handleOnSubmit(event)}>
-
-//                     <label>YOUR NAME</label>
-//                     <input type='text' placeholder="Nhập"
-//                         onChange={(event) => this.handleOnChangeInput_Name(event)}
-//                         value={this.state.name}>
-//                     </input>
-//                     <div></div>
-//                     <label>YOUR AGE</label>
-//                     <input type='text' placeholder="Nhập"
-//                         onChange={(event) => this.handleOnChangeInput_Age(event)}
-//                         value={this.state.age}>
-//                     </input>
-
-//                     <button>Submit</button>
-//                 </form>
-//             </div>
-//         )
-//     }
-// }
 
 
 
@@ -65,6 +7,7 @@ const AddUserInfor = (props) => {
 
     const [name, setName] = useState('')
     const [age, setAge] = useState('')
+
 
     const handleOnChangeInput_Name = (event) => {
         setName(event.target.value)
