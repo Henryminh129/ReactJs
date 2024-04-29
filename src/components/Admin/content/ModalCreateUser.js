9fd5d15f5fa055f282f5ad905d521c95c122ddc2
Henryminh129 <156094841+Henryminh129@users.noreply.github.com>
2024-04-29 08:59:06 +0700

    new

--- src/components/Admin/content/ModalCreateUser.js
+++ src/components/Admin/content/ModalCreateUser.js
@@ -6,8 +6,25 @@ import { FcPlus } from 'react-icons/fc'
 const ModalCreateNewUser = () => {
     const [show, setShow] = useState(false)
 
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [username, setUsername] = useState("");
+    const [image, setImage] = useState("");
+    const [role, setRole] = useState("USER")
+    const [previewImage, setPreviewImage] = useState("")
+
     const handleClose = () => setShow(false)
     const handleShow = () => setShow(true)
+    const handleUploadImage = (event) => {
+        if (event.target && event.target.files && event.target.files[0]) {
+            setPreviewImage(URL.createObjectURL(event.target.files[0]))
+            setImage(event.target.files[0])
+        } else {
+            // setPreviewImage("")
+        }
+
+    }
+
 
 
     return (
@@ -24,32 +41,36 @@ const ModalCreateNewUser = () => {
                     <form className="row g-3">
                         <div className="col-md-6">
                             <label className="form-label">Email</label>
-                            <input type="email" className="form-control" />
+                            <input type="email" className="form-control" value={email}
+                                onChange={(event) => { setEmail(event.target.value) }} />
                         </div>
                         <div className="col-md-6">
                             <label className="form-label">Password</label>
-                            <input type="password" className="form-control" />
+                            <input type="password" className="form-control" value={password}
+                                onChange={(event) => { setPassword(event.target.value) }} />
                         </div>
                         <div className="col-md-6">
                             <label className="form-label">User's name</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" value={username}
+                                onChange={(event) => { setUsername(event.target.value) }} />
                         </div>
                         <div className="col-md-4">
                             <label className="form-label">Role</label>
-                            <select className="form-select">
-                                <option selected value="USER">USER</option>
-                                <option selected value="ADMIN">ADMIN</option>
+                            <select className="form-select" onChange={(event) => setRole(event.target.value)}>
+                                <option value="USER">USER</option>
+                                <option value="ADMIN">ADMIN</option>
                             </select>
                         </div>
                         <div className='col-md-12'>
-                            <label className="form-label label-upload" htmlFor='lableUpload'><FcPlus /> Upload File Image </label>
-                            <input type='file' hidden id="lableUpload" />
+                            <label className="form-label label-upload" htmlFor='labelUpload'><FcPlus /> Upload File Image </label>
+                            <input type='file' hidden id="labelUpload" onChange={(event) => handleUploadImage(event)} />
                         </div>
 
                         <div className='col-md-12 img-preview'>
-                            <span>Preview Image</span>
-                            <img></img>
-
+                            {previewImage ?
+                                <img src={previewImage}></img> :
+                                <span >Preview Image</span>
+                            }
                         </div>
                     </form>
                 </Modal.Body>
