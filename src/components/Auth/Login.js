6c6bb207a1ed90a639aae2caa90e170c5c36dc00
Henryminh129 <156094841+Henryminh129@users.noreply.github.com>
2024-05-18 06:39:57 +0700

    new

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,7 +1,43 @@
+import { useState } from 'react';
+import './Login.scss'
+
+
 const Login = (props) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("")
+    const handleLogin = props.handleLogin
     return (
-        <div>
-            login component
+        <div className='login-container'>
+            <div className='header'>
+                Don't have an account yet?
+            </div>
+            <div className='title col-4 mx-auto'>
+
+            </div>
+            <div className='welcome col-4 mx-auto'>
+                Hello, who's this?
+            </div>
+            <div className='content-form col-4 mx-auto'>
+                <div className='form-group'>
+                    <label>Email</label>
+                    <input
+                        type={'email'}
+                        className='form-control'
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
+                    ></input>
+                </div>
+                <div className='form-group'>
+                    <label>Password</label>
+                    <input type={'password'} className='form-control' value={password} onChange={(event) => setPassword(event.target.value)}></input>
+                </div>
+                <span>Forgot password?</span>
+                <div>
+                    <button className='btn-submit' onClick={() => handleLogin()}>Login to</button>
+                </div>
+
+            </div>
+
         </div>
     )
 }
